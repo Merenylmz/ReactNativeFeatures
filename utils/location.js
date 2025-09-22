4f9507b0d9ce913c352be70b79d281658e51bbd9
Muhammet Eren <115183696+Merenylmz@users.noreply.github.com>
2025-09-22 09:15:31 +0300

    Security Problem

--- utils/location.js
+++ utils/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyDNZl9QtnMF3gTNQtHtjuOUmxUMl5XDbQE";
+const GOOGLE_API_KEY = "GOOGLE_API_KEY";
 
 const getMapPreview = (lat, lng) =>{
     const mapPreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
